7ceb16c2132c827e701fcb65bcf9887c7ba2acd4
Paul Bie <pbie@student.42.fr>
2018-04-29 13:53:39 +0100

    [Refactor] - Lobby component

--- src/client/components/lobby/Lobby.js
+++ src/client/components/lobby/Lobby.js
@@ -1,5 +1,90 @@
 import React, { Component } from 'react'
 
+function ChatComponent(props) {
+	const C = new Component()
+
+	C.render = () => {
+		return (
+			<div className="lobby-chat-new">
+				<div>
+					<div>
+						<div>
+							<h1>Chat</h1>
+						</div>
+					</div>
+					<div>
+						<div>
+							<h2>test</h2>
+						</div>
+					</div>
+					<div>
+						<input type="text" name="" id="" placeholder="Enter message" />
+					</div>
+				</div>
+				<div />
+				<div>
+					<div>
+						<div>
+							<h1>New Room</h1>
+						</div>
+					</div>
+					<div className="new-room-inputs">
+						<div>
+							<div>
+								<input type="text" name="" id="" placeholder="Room Name" />
+							</div>
+							<div>
+								<input type="text" name="" id="" placeholder="Room Name" />
+							</div>
+							<div>
+								<input type="text" name="" id="" placeholder="Room Name" />
+							</div>
+						</div>
+					</div>
+					<div>
+						<div>
+							<div>Create</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+	return C
+}
+
+function RoomsComponent(props) {
+	const C = new Component()
+
+	C.render = () => {
+		return (
+			<div className="lobby-rooms">
+				<div>
+					<div />
+					<div>
+						<div>
+							<div>
+								<h1>Rooms</h1>
+							</div>
+						</div>
+						<div>
+							<div>
+								<h2>test</h2>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div>+</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+	return C
+}
+
 function LobbyComponent(props) {
 	const C = new Component()
 
@@ -10,58 +95,8 @@ function LobbyComponent(props) {
 	C.render = () => {
 		return (
 			<div className="container-lobby">
-				<div className="lobby-chat-new">
-					<div>
-						<div>
-							<div>
-								<h1>Chat</h1>
-							</div>
-						</div>
-						<div>
-							<div>
-								<h2>test</h2>
-							</div>
-						</div>
-						<div>
-							<input type="text" name="" id="" placeholder="Enter message" />
-						</div>
-					</div>
-					<div />
-					<div>
-						<div>
-							<div>
-								<h1>New Room</h1>
-							</div>
-						</div>
-						<div className="new-room-inputs">
-							<div>
-								<div>
-									<input type="text" name="" id="" placeholder="Room Name" />
-								</div>
-								<div>
-									<input type="text" name="" id="" placeholder="Room Name" />
-								</div>
-								<div>
-									<input type="text" name="" id="" placeholder="Room Name" />
-								</div>
-							</div>
-						</div>
-						<div>
-							<div>
-								<div>Create</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="lobby-rooms">
-					<div>
-						<div>
-							<div>
-								<h1>Rooms</h1>
-							</div>
-						</div>
-					</div>
-				</div>
+				<RoomsComponent />
+				<ChatComponent />
 			</div>
 		)
 	}
