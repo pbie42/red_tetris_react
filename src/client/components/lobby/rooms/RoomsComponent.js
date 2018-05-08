52609f3f3ce66ed1985cd02de29e5634ac86bbc2
Paul Bie <pbie@student.42.fr>
2018-05-08 11:57:18 +0200

    [Wip] - Netflix like carousel for joinable rooms

--- src/client/components/lobby/rooms/RoomsComponent.js
+++ src/client/components/lobby/rooms/RoomsComponent.js
@@ -1,7 +1,55 @@
 import React, { Component } from 'react'
 
+const rooms = [
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] },
+	{ name: 'test1', people: ['john', 'paul', 'george', 'ringo'] }
+]
+
 export function RoomsComponent(props) {
 	const C = new Component()
+	let div
+
+	C.componentDidMount = function() {
+		C.scrollToLeft()
+	}
+
+	C.componentWillUpdate = function() {
+		C.scrollToLeft()
+	}
+
+	C.scrollToLeft = function() {
+		const { scrollWidth, clientWidth } = div
+		const maxScrollTop = scrollWidth - clientWidth
+		div.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0
+	}
 
 	C.render = () => {
 		return (
@@ -15,13 +63,19 @@ export function RoomsComponent(props) {
 							</div>
 						</div>
 						<div>
-							<div>
-								<h2>test</h2>
+							<div ref={node => (div = node)}>
+								{rooms.map(room => (
+									<div>
+										<h1>{room.name}</h1>
+									</div>
+								))}
 							</div>
 						</div>
 					</div>
 					<div>
-						<div onClick={() => C.props.showNewRoom()}>+</div>
+						<div onClick={() => C.props.showNewRoom()}>
+							<h1>+</h1>
+						</div>
 					</div>
 				</div>
 			</div>
