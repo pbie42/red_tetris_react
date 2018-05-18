a7b34a4d95f1f07ce375e8216de3f9d61ab11288
Paul Bie <pbie@student.42.fr>
2018-05-18 14:19:38 +0200

    [Fix] - Error page animation transitions

--- src/client/components/error/ErrorComponent.js
+++ src/client/components/error/ErrorComponent.js
@@ -3,20 +3,38 @@ import React, { Component } from 'react'
 function ErrorComponent(props) {
 	const C = new Component(props)
 
+	C.state = {
+		change: false
+	}
+
 	C.componentDidMount = function() {
+		function delayFade() {
+			C.setState({ change: true })
+		}
+		setTimeout(delayFade, 4000)
 		function delayRouteChange() {
 			props.history.push('/')
 		}
-		setTimeout(delayRouteChange, 5000)
+		setTimeout(delayRouteChange, 6000)
 	}
 
 	C.render = () => {
 		return (
-			<div className="container-error">
+			<div
+				className={
+					!C.state.change ? 'container-error' : 'container-error container-fade'
+				}
+			>
 				{!C.props.errorName &&
 					C.props.roomName &&
 					!C.props.error && (
-						<div className="error-message-container">
+						<div
+							className={
+								!C.state.change
+									? 'error-message-container'
+									: 'error-message-container moveOutDivDown'
+							}
+						>
 							<h1>ERROR</h1>
 							<h2>Room Name Taken</h2>
 							<h3>You will be redirected now!</h3>
@@ -25,7 +43,13 @@ function ErrorComponent(props) {
 				{C.props.errorName &&
 					!C.props.roomName &&
 					!C.props.error && (
-						<div className="error-message-container">
+						<div
+							className={
+								!C.state.change
+									? 'error-message-container'
+									: 'error-message-container moveOutDivDown'
+							}
+						>
 							<h1>ERROR</h1>
 							<h2>Username Taken</h2>
 							<h3>You will be redirected now!</h3>
@@ -35,7 +59,13 @@ function ErrorComponent(props) {
 					!C.props.roomName &&
 					!C.props.errorTooManyMembers &&
 					!C.props.error && (
-						<div className="error-message-container">
+						<div
+							className={
+								!C.state.change
+									? 'error-message-container'
+									: 'error-message-container moveOutDivDown'
+							}
+						>
 							<h1>ERROR</h1>
 							<h2>You shouldn't be here lol</h2>
 							<h3>You will be redirected now!</h3>
@@ -45,7 +75,13 @@ function ErrorComponent(props) {
 					!C.props.roomName &&
 					!C.props.error &&
 					C.props.errorTooManyMembers && (
-						<div className="error-message-container">
+						<div
+							className={
+								!C.state.change
+									? 'error-message-container'
+									: 'error-message-container moveOutDivDown'
+							}
+						>
 							<h1>ERROR</h1>
 							<h2>Too Many Members</h2>
 							<h3>You will be redirected now!</h3>
@@ -54,7 +90,13 @@ function ErrorComponent(props) {
 				{!C.props.errorName &&
 					!C.props.roomName &&
 					C.props.error && (
-						<div className="error-message-container">
+						<div
+							className={
+								!C.state.change
+									? 'error-message-container'
+									: 'error-message-container moveOutDivDown'
+							}
+						>
 							<h1>ERROR</h1>
 							<h2>Other</h2>
 							<h3>You will be redirected now!</h3>
