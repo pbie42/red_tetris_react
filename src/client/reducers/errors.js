6a6b8c2326ca6265227f34a21006071100a66a9b
Paul Bie <pbie@student.42.fr>
2018-05-20 17:29:00 +0200

    [Refactor] - ROOMNAME_TAKEN to ERROR_ROOMNAME_TAKEN

--- src/client/reducers/errors.js
+++ src/client/reducers/errors.js
@@ -11,7 +11,7 @@ const errors = (state = initialErrorState, action) => {
 	switch (action.type) {
 		case types.ERROR_USERNAME_TAKEN:
 			return { ...state, errorName: true }
-		case types.ROOMNAME_TAKEN:
+		case types.ERROR_ROOMNAME_TAKEN:
 			return { ...state, errorRoom: true }
 		case types.ERROR_TOO_MANY_MEMBERS:
 			return { ...state, errorTooManyMembers: true }
