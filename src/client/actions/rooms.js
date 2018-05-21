87e6aa60ab8a439aa0033ff46e96835f6a08cf40
Paul Bie <pbie@student.42.fr>
2018-05-21 12:36:27 +0200

    [Refactor] - Alphabetized rooms actions

--- src/client/actions/rooms.js
+++ src/client/actions/rooms.js
@@ -1,21 +1,15 @@
 import * as types from '../constants/ActionTypes'
 
-export const roomAddUser = (username, roomName) => ({
-	type: types.ROOM_ADD_USER,
-	roomName,
-	username
-})
-
-export const roomRemoveUser = (username, roomName) => ({
-	type: types.ROOM_REMOVE_USER,
-	roomName,
-	username
-})
-
 export const roomAdd = (roomName, members) => ({
 	type: types.ROOM_ADD_ROOM,
 	roomName,
 	members
+})
+
+export const roomAddUser = (username, roomName) => ({
+	type: types.ROOM_ADD_USER,
+	roomName,
+	username
 })
 
 export const roomAdded = (roomName, members) => ({
@@ -24,12 +18,18 @@ export const roomAdded = (roomName, members) => ({
 	members
 })
 
-export const roomsPopulateList = rooms => ({
-	type: types.ROOMS_LIST,
-	rooms
+export const roomRemoveUser = (username, roomName) => ({
+	type: types.ROOM_REMOVE_USER,
+	roomName,
+	username
 })
 
 export const roomsListReceived = () => ({
 	type: types.ROOMS_LIST_RECEIVED,
 	rooms: true
+})
+
+export const roomsPopulateList = rooms => ({
+	type: types.ROOMS_LIST,
+	rooms
 })
